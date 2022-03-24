770a92c02cb497d8bceaa773d89e4621e163451b
Code <a@b.com>
2022-03-24 08:51:58 +0000

    Updated: 2022-03-24 08:51:58

--- utils/trainer.py
+++ utils/trainer.py
@@ -163,7 +163,10 @@ def train_distilled(epoch, train_loader, module_list, criterion_list, optimizer,
         else:
             loss_cls = F.cross_entropy(logit_s, target.argmax(dim=-1), reduction='mean')       
 
-        loss = config.w_ce * loss_cls + config.w_kl * ensemble_loss + config.w_other * loss_kd
+        if config.w_kl < 0:
+            loss = config.w_ce * loss_cls + config.teachers * ensemble_loss + config.w_other * loss_kd
+        else:
+            loss = config.w_ce * loss_cls + config.w_kl * ensemble_loss + config.w_other * loss_kd
 
         total_kl += batch_loss
         total_ce_loss += loss_cls
@@ -228,7 +231,10 @@ def validation(epoch, val_loader, module_list, criterion_list, optimizer, config
         teacher_losses, ensemble_weights = model_weights(teachers_loss,train=False)
         ensemble_loss = torch.sum(teacher_losses)
 
-        loss = config.w_ce * loss_cls + config.w_kl * ensemble_loss + config.w_other * loss_kd
+        if config.w_kl < 0:
+            loss = config.w_ce * loss_cls + config.teachers * ensemble_loss + config.w_other * loss_kd
+        else:
+            loss = config.w_ce * loss_cls + config.w_kl * ensemble_loss + config.w_other * loss_kd
 
         total_kl += batch_loss
         total_ce_loss += loss_cls

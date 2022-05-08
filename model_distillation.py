295b80063f823bfed7c39d9af0b833e4eb8350f3
Code <a@b.com>
2022-05-08 13:50:00 +0000

    Updated: 2022-05-08 13:50:00

--- model_distillation.py
+++ model_distillation.py
@@ -57,6 +57,47 @@ def Run_NN_Teacher(model, config):
                 torch.save(model.state_dict(), savepath)
 
 
+# In[2]:
+
+
+def Run_SK_Teacher(config):
+    from sktime.datatypes._panel._convert import from_2d_array_to_nested
+    training, testing = get_raw_data(config)
+    
+    X_train = from_2d_array_to_nested(training.x.squeeze().cpu().detach().numpy())
+    y_train = training.y.squeeze().cpu().detach().numpy()
+    
+    X_test = from_2d_array_to_nested(testing.x.squeeze().cpu().detach().numpy())
+    y_test = testing.y.squeeze().cpu().detach().numpy()
+
+    if config.teacher_type == 'CIF':
+        from sktime.classification.interval_based import CanonicalIntervalForest
+        classifier = CanonicalIntervalForest(random_state=config.init_seed)
+    elif config.teacher_type == 'Forest':
+        from sktime.classification.interval_based import TimeSeriesForestClassifier
+        classifier = TimeSeriesForestClassifier(random_state=config.init_seed)
+    elif config.teacher_type == 'Proximity':
+        from sktime.classification.distance_based import ProximityTree
+        classifier = ProximityTree(random_state=config.init_seed)
+    elif config.teacher_type == 'TDE':
+        from sktime.classification.dictionary_based import IndividualTDE
+        classifier = IndividualTDE(random_state=config.init_seed)
+    elif config.teacher_type == 'Rocket':
+        from sktime.classification.kernel_based import RocketClassifier
+        classifier = RocketClassifier(random_state=config.init_seed)
+    elif config.teacher_type == 'Matrix':
+        from sktime.classification.feature_based import MatrixProfileClassifier
+        classifier = MatrixProfileClassifier(random_state=config.init_seed)
+    
+    classifier.fit(X_train, y_train)
+    
+    model_name = f'{config.teacher_type}_{config.experiment}_{config.init_seed}_teacher.pkl'
+    savepath = "./teachers/" + model_name
+    
+    with open(savepath,'wb') as file:
+        pickle.dump(classifier,file)
+
+
 # In[4]:
 
 
@@ -260,49 +301,6 @@ def TeacherEvaluation(config):
         config.batch_size = 10000
         _, _, test_loader = get_loaders(config)
     evaluate_ensemble(test_loader, config)
-
-
-# In[2]:
-
-
-def Run_SK_Teacher(config):
-    from sktime.datatypes._panel._convert import from_2d_array_to_nested
-    training, testing = get_raw_data(config)
-    
-    X_train = from_2d_array_to_nested(training.x.squeeze().cpu().detach().numpy())
-    y_train = training.y.squeeze().cpu().detach().numpy()
-    
-    X_test = from_2d_array_to_nested(testing.x.squeeze().cpu().detach().numpy())
-    y_test = testing.y.squeeze().cpu().detach().numpy()
-
-    if config.teacher_type == 'CIF':
-        from sktime.classification.interval_based import CanonicalIntervalForest
-        classifier = CanonicalIntervalForest(random_state=config.init_seed)
-    elif config.teacher_type == 'Forest':
-        from sktime.classification.interval_based import TimeSeriesForestClassifier
-        classifier = TimeSeriesForestClassifier(random_state=config.init_seed)
-    elif config.teacher_type == 'Proximity':
-        from sktime.classification.distance_based import ProximityTree
-        classifier = ProximityTree(random_state=config.init_seed)
-    elif config.teacher_type == 'TDE':
-        from sktime.classification.dictionary_based import IndividualTDE
-        classifier = IndividualTDE(random_state=config.init_seed)
-    elif config.teacher_type == 'Rocket':
-        from sktime.classification.kernel_based import RocketClassifier
-        classifier = RocketClassifier(random_state=config.init_seed)
-    elif config.teacher_type == 'Matrix':
-        from sktime.classification.feature_based import MatrixProfileClassifier
-        classifier = MatrixProfileClassifier(random_state=config.init_seed)
-    
-    classifier.fit(X_train, y_train)
-    
-    model_name = f'{config.teacher_type}_{config.experiment}_{config.init_seed}_teacher.pkl'
-    savepath = "./teachers/" + model_name
-    
-    with open(savepath,'wb') as file:
-        pickle.dump(classifier,file)
-        
-    TeacherEvaluation(config)
 
 
 # In[6]:
@@ -605,6 +603,7 @@ if __name__ == '__main__':
             torch.cuda.manual_seed(teacher)
             torch.backends.cudnn.deterministic = True
             Run_SK_Teacher(config)
+        TeacherEvaluation(config)
     elif config.evaluation == 'teacher_ensemble':
         TeacherEvaluation(config)
     elif config.evaluation == 'student_bo' or config.evaluation == 'student_bo_simple':
